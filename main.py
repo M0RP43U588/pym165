1a70a221d9ac5d2fdc6ad628c7c824db97b909eb
M0RP43U588 <lukasthommen06@gmail.com>
2025-10-26 19:18:28 +0100

    css is already ragebaiting, made a bit more responsive then decided to just quit the program if a certain terminal size is not met

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from os import get_terminal_size
+from sys import exit as leave
 from tui import Tui
 
 
+def termsize_check() -> bool:
+    termsize: tuple = tuple(get_terminal_size())
+    return bool(termsize[0] >= 180 and termsize[1] >= 50)
+
+
 def main() -> None:
+    if not termsize_check():
+        leave("Terminal muss mindestens 180x50 sein")
+
     app: Tui = Tui()
     app.run()
 

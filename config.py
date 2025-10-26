eb29b17ed437f3db54cb6ba79cc9af900bec98fd
M0RP43U588 <lukasthommen06@gmail.com>
2025-10-26 18:21:08 +0100

    progress
    - added config.py containing resources such as multiline strings or
    version information
    - split the tcss files into two, one containing all the classes or multi
    selectors and the other one containing ids for specific widget styling
    - added config.py to ignore in ruff configuration
    - made a short navigation guide and a mongodb ascii art text in the
    welcome widgets

---
+++ config.py
@@ -0,0 +1,24 @@
+version: str = "0.1.0"
+
+mongodb_ascii = r"""
+ /$$      /$$                                         /$$$$$$$  /$$$$$$$ 
+| $$$    /$$$                                        | $$__  $$| $$__  $$
+| $$$$  /$$$$  /$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$ | $$  \ $$| $$  \ $$
+| $$ $$/$$ $$ /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$| $$  | $$| $$$$$$$ 
+| $$  $$$| $$| $$  \ $$| $$  \ $$| $$  \ $$| $$  \ $$| $$  | $$| $$__  $$
+| $$\  $ | $$| $$  | $$| $$  | $$| $$  | $$| $$  | $$| $$  | $$| $$  \ $$
+| $$ \/  | $$|  $$$$$$/| $$  | $$|  $$$$$$$|  $$$$$$/| $$$$$$$/| $$$$$$$/
+|__/     |__/ \______/ |__/  |__/ \____  $$ \______/ |_______/ |_______/ 
+                                  /$$  \ $$                              
+                                 |  $$$$$$/                              
+                                  \______/
+"""
+
+help_text = """
+Willkommen, die navigation funktioniert wie folgt:
+    - Tab zum auswählen der Buttons (nächster)
+    - Shift Tab zum auswählen der Buttons (vorheriger)
+    - Enter um einen Button zu klicken
+    - Control+Q um das Programm zu beenden
+    - Der Mauszeiger kann auch verwendet werden
+"""
--- tui.py
+++ tui.py
@@ -1,23 +1,33 @@
 from textual.app import App, ComposeResult
 from textual.containers import VerticalGroup, Horizontal
 from textual.widgets import Footer, Header, Button, Static
+from config import version, mongodb_ascii, help_text
 
 
 class CrudButtons(VerticalGroup):
     def compose(self) -> ComposeResult:
-        yield Button("Create", id="create_button")
-        yield Button("Read", id="read_button")
-        yield Button("Update", id="update_button")
-        yield Button("Delete", id="delete_button")
+        yield Button("Create",
+                     id="create_button", classes="crud_buttons")
+        yield Button("Read",
+                     id="read_button", classes="crud_buttons")
+        yield Button("Update",
+                     id="update_button", classes="crud_buttons")
+        yield Button("Delete",
+                     id="delete_button", classes="crud_buttons")
 
 
 class WelcomeScreen(VerticalGroup):
     def compose(self) -> ComposeResult:
-        yield Static("version placeholder - PyMongo TUI", id="version_static")
+        yield Static(f"{version} - PyM165 TUI",
+                     classes="welcome_widgets", id="version_static")
+        yield Static(mongodb_ascii,
+                     classes="welcome_widgets", id="mongodb_ascii")
+        yield Static(help_text,
+                     classes="welcome_widgets", id="help_text")
 
 
 class Tui(App):
-    CSS_PATH = "tui.tcss"
+    CSS_PATH = ["classes.tcss", "ids.tcss"]
 
     def on_mount(self) -> None:
         self.theme: str = "catppuccin-mocha"
